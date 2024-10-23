40c2a2515e54435e7b5cd2bb1ec0658920daa0fe
Yunya <alice504119@gmail.com>
2024-10-23 23:12:31 +0800

    Updates

---
+++ assets/index-358f305a.js
@@ -0,0 +1,1 @@
+import"./main-36c65608.js";new Swiper(".swiper",{pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
