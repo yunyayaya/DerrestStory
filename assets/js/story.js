8c24c7a571c04ffe66f252a9e13c838f8f0d7608
Lotte <nieves957@gmail.com>
2024-10-27 03:10:50 +0800

    [Lotte]更新swiperRWD

--- assets/js/story.js
+++ assets/js/story.js
@@ -1,34 +1,41 @@
-//這個檔案用來客製輪播
-
-//html檔案引入class="swiper-control" 有改名字這裡一定要改!!不然會吃不到
-const swiper = new Swiper('.swiper-control', {
-  slidesPerView: 'auto', //顯示的卡片數量
-  spaceBetween: 40,     //每個輪播卡片間距40px
-  freeMode: true,      //自由模式
-  
-  loop: true, // 讓輪播循環
-  //分頁小圓點
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true, // 點分頁小圓點時切換頁面
+const bannerSwiper = new Swiper(".insp-swiper", {
+  loop: true,
+  speed: 5000,
+  freeMode: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true, 
   },
-  //左右切換箭頭
+  centeredSlides: true,
+  slidesPerView: 2, //設定每次在畫面上顯示 2張slide
+  breakpoints: {
+    375: {
+      slidesPerView: 1.4,
+      spaceBetween: 16,
+    },
+    576: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 1.4
+    },
+    992: {
+      slidesPerView: 2
+    },
+    1080: {
+      slidesPerView: 2.3
+    }
+  },
+  spaceBetween: 35, //slide間距40
+  // effect: 'slide',
+  pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  //滾動條
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true, // 滾動條可拖動
-  },
-  //自動播放
-    autoplay: {
-     //3秒後自動波放下一張 
-     delay: 3000,
-     // 使用者點擊後不停止自動播放
-    disableOnInteraction: false,   
-  },
 });
-// 用來確認我的頁面有沒有吃到js檔案
-console.log("Swiper initialized"); 
+
+console.log("有顯示出來，確定有載入JS")
