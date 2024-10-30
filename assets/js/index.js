020d1021fbd28a7acb5edf3b997e62c6f8e38930
Chun <d19930216@hotmail.com>
2024-10-30 23:13:38 +0800

    調整首頁的各個細節

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper('.swiper', {
+const indexBannerSwiper = new Swiper('.banner-swiper', {
+  loop: true,
   // 分頁、左右箭頭、滾動條若有使用則必需設定          
   // 分頁   
   pagination: {
@@ -6,18 +7,19 @@ const swiper = new Swiper('.swiper', {
   },
   // 左右箭頭    
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  // 滾動條
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: '.banner-swiper-button-next',
+    prevEl: '.banner-swiper-button-prev',
   },
 });
 
 
-const sectionTopPickSwiper = new Swiper(".section-top-pick-swiper-container", {
+const sectionTopPickSwiper = new Swiper(".section-top-pick-swiper", {
   slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    },
+  },
   spaceBetween: 24,
   loop: true,
   pagination: {
@@ -27,6 +29,36 @@ const sectionTopPickSwiper = new Swiper(".section-top-pick-swiper-container", {
   navigation: {
     nextEl: '.section-top-pick-swiper-button-next',
     prevEl: '.section-top-pick-swiper-button-prev',
+  },
+  autoHeight: true,
+  watchOverflow: true,
+});
+
+const sectionDessertStorySwiper = new Swiper(".section-dessert-story-swiper", {
+  slidesPerView: 1,
+  loop: true,
+  speed: 1000,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      loopedSlides: 2, // 中等螢幕設為 2
+    },
+    768: {
+      slidesPerView: 3,
+      loopedSlides: 3, // 大螢幕設為 3
+    },
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  spaceBetween: 24,
+  pagination: {
+    el: ".section-dessert-story-swiper-pagination",
+    clickable: true, 
+  },
+  navigation: {
+    nextEl: '.section-dessert-story-swiper-button-next',
+    prevEl: '.section-dessert-story-swiper-button-prev',
   },
   autoHeight: true,
   watchOverflow: true,
